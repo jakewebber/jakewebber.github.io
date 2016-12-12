9c9926ecde5124c1b44cbfbc6b0529b1e3e63abb
Jake Webber <jacobwwebber@gmail.com>
2016-12-11 21:21:40 -0500

    Nav update

--- js/followmenu.js
+++ js/followmenu.js
@@ -7,9 +7,9 @@ $(document).ready(function () {
         $(document).off("scroll");
         
         $('a').each(function () {
-            $(this).removeClass('active');
+            $(this).removeClass('current');
         })
-        $(this).addClass('active');
+        $(this).addClass('current');
       
         var target = this.hash,
             menu = target;
@@ -28,12 +28,12 @@ function onScroll(event){
     $('#menu-center a').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('#menu-center ul li a').removeClass("active");
-            currLink.addClass("active");
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos-200) {
+            $('#menu-center ul li a').removeClass("current");
+            currLink.addClass("current");
         }
         else{
-            currLink.removeClass("active");
+            currLink.removeClass("current");
         }
     });
 }
